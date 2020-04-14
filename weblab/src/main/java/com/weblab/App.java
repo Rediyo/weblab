03f8deec6256080d285b80f7383b04e3853c2b90
idouz <idouz@qq.com>
2020-04-14 13:34:28 +0800

    Web Lab

---
+++ weblab/src/main/java/com/weblab/App.java
@@ -0,0 +1,35 @@
+package com.weblab;
+import java.io.*;
+import java.util.*;
+import org.apache.commons.codec.digest.DigestUtils;
+public class App {
+public static void main(String[] args) throws IOException{
+        File file = new File("src/password.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        Scanner s = new Scanner(System.in);
+        String[] strs = new String [2];
+        System.out.print("输入账号：");
+        strs[0] = s.nextLine();
+        System.out.print("输入密码：");
+        strs[1] = s.nextLine();
+        StringBuffer bf = new StringBuffer();
+        String str = "";
+        int i = 0;
+        boolean flag=true;
+        while ((str = br.readLine()) != null) {
+            if (!sha256hex(str).equals(sha256hex(strs[i++]))) {
+	flag=false;
+             	break;
+            }
+        }
+	if(flag){
+	System.out.println("登录成功");	
+	}else{
+	System.out.println("登录失败");
+	}
+        br.close();
+    }
+    public static String sha256hex(String input) {
+        return DigestUtils.sha256Hex(input);
+    }
+}
